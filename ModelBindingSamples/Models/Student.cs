75181c69179e470b86e96f8d799c785019415d54
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-09-05 22:16:00 -0700

    Add project files.

---
+++ ModelBindingSamples/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModelBindingSamples.Models
+{
+
+    public class Student
+    {
+        [Key] //Primary Key
+        public int StudentId { get; set; }
+    
+        public string FullName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
