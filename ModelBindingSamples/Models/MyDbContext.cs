5c8c02b804ac5a7112ab9213266531fb25e18c7b
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-09-05 22:22:33 -0700

    Add student model to the context

--- ModelBindingSamples/Models/MyDbContext.cs
+++ ModelBindingSamples/Models/MyDbContext.cs
@@ -4,6 +4,14 @@ namespace ModelBindingSamples.Models
 {
     public class MyDbContext : DbContext
     {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
 
+        // Add a DbSet for each entity to be tracked by the context
+        // The context will create tables for each DBSet and
+        // create migrations to update the database schema
+        public DbSet<Student> Students { get; set; }
     }
 }
